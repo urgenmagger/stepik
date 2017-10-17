7b4947b23018ca209fa57827f2190de046d5149a
urgenmagger <evgen.magger@gmail.com>
2017-10-17 08:27:14 +0300

    restoring

---
+++ Merge.java
@@ -0,0 +1,31 @@
+package stepic_2;
+
+import java.util.Arrays;
+
+public class Merge {
+    public static void main(String[] args) {
+        int a1[] = {1, 2, 3, 4};
+        int a2[] = {1, 5, 7, 9};
+        int a3[] = mergeArrays(a1, a2);
+        System.out.println(Arrays.toString(a3));
+    }
+
+    public static int[] mergeArrays(int[] a1, int[] a2) {
+        int result[] = new int[a1.length + a2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < a1.length && j < a2.length) {//both arrays
+            if (a1[i] < a2[j]) {
+                result[k++] = a1[i++];
+            } else {
+                result[k++] = a2[j++];
+            }
+        }
+        while (i < a1.length) { //only one array
+            result[k++] = a1[i++];
+        }
+        while (j < a2.length) {
+            result[k++] = a2[j++];
+        }
+        return result;
+    }
+}
---
+++ Play.java
@@ -0,0 +1,32 @@
+package stepic_2;
+
+public class Play {
+
+    public static void main(String[] args) {
+        String[] roles = {
+                "Городничий", "Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич"};
+        String[] textLines = {
+                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+
+        String result = printTextPerRole(roles, textLines);
+        System.out.println(result);
+    }
+
+
+    public static String printTextPerRole(String[] roles, String[] textLines) {
+        for (int i = 0; i <= roles.length; i++){
+            for (int j = 0; j <= textLines.length; i++) {
+                
+            }
+        }
+        return "";
+    }
+}
