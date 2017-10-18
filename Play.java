fb2f83f60383e7b1b449f480e2f7dc81737f148c
urgenmagger <evgen.magger@gmail.com>
2017-10-18 22:59:49 +0300

    task_role_refactor

--- Play.java
+++ Play.java
@@ -17,7 +17,6 @@ public class Play {
                 "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
 
         String result = printTextPerRole(roles, textLines);
-        System.out.println(result);
     }
 
 
